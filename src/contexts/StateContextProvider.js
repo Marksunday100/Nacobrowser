6bd2bea348c276f409965d8812f80f9ed1d0b073
Marksunday100 <116458183+Marksunday100@users.noreply.github.com>
2023-03-10 16:13:07 +0100

    Update StateContextProvider.js

--- src/contexts/StateContextProvider.js
+++ src/contexts/StateContextProvider.js
@@ -15,7 +15,7 @@ export const StateContextProvider = ({ children }) => {
       method: 'GET',
       headers: {
         'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+        'x-rapidapi-key': 'd78204c485msh8e7bef6fb273d60p1264fcjsnaa64b1654664',
       },
     });
 
